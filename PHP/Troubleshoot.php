8bec26db5b4769883d5aeedf552d0b3f99a39d95
simikraemer <simonkraemer@weh.rwth-aachen.de>
2024-12-02 08:07:04 +0100

    Troubleshoot.php Mobile Format Fix

--- PHP/Troubleshoot.php
+++ PHP/Troubleshoot.php
@@ -46,7 +46,6 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="WEH.css" media="screen">
 </head>
 
