2ffe8b9ebf87f3835f479f381b594e3e07e779dc
simikraemer <simonkraemer@weh.rwth-aachen.de>
2025-07-23 19:57:12 +0200

    airfryer2

--- PHP/Erstattungsantrag.php
+++ PHP/Erstattungsantrag.php
@@ -212,12 +212,13 @@ load_menu();
 
 
     <div style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 0.5em;">
-      <div style="padding: 0.5em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Wasserkocher</div>
-      <div style="padding: 0.5em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Mikrowelle</div>
-      <div style="padding: 0.5em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Staubsauger</div>
-      <div style="padding: 0.5em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Kaffeemaschine</div>
-      <div style="padding: 0.5em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Insektennetz</div>
-      <div style="padding: 0.5em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Toaster</div>
+      <div style="padding: 0.3em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Wasserkocher</div>
+      <div style="padding: 0.3em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Mikrowelle</div>
+      <div style="padding: 0.3em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Staubsauger</div>
+      <div style="padding: 0.3em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Kaffeemaschine</div>
+      <div style="padding: 0.3em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Insektennetz</div>
+      <div style="padding: 0.3em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Toaster</div>
+      <div style="padding: 0.3em; border: 1px solid #11a50d; border-radius: 4px; text-align: center;">Airfryer/Fritteuse</div>
     </div>
 
     <hr style="border: none; border-top: 2px solid #11a50d; margin: 1.3em 0;">
